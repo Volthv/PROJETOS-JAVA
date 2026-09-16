736c967792be7df146330a71348647dd70ecbdf6
Haryson <hcamargoholanda@gmail.com>
2026-09-16 20:07:27 -0300

    bagulho de empresa e tal
    empresa a da silva

--- Main.java
+++ Main.java
@@ -1,29 +1,93 @@
 
-class Animal {
-    protected String Animal;
-    protected String animaltype;
-    protected int idade;
 
-    public Animal(String Animal, String animaltype, int idade) {
-        this.Animal = Animal;
-        this.animaltype = animaltype;
-        this.idade = idade;
+//classe de empresa
+class Empresarial {
+    protected String NomedaEmpresa;
+    protected Integer PrecoMensagem;
+    protected Integer PrecoEMAIL;
+    protected Integer Disparos;
+    protected Integer PrecoTotal;
+    protected Integer descontoMensagem;
+    /*Transiçao pra info protegida pra publica*/
+    public Empresarial(String NomedaEmpresa, Integer PrecoMensagem, Integer PrecoEMAIL, Integer Disparos, Integer PrecoTotal) {
+        this.NomedaEmpresa = NomedaEmpresa;
+        this.PrecoMensagem = PrecoMensagem;
+        this.PrecoEMAIL = PrecoEMAIL;
+        this.Disparos = Disparos;
+        this.PrecoTotal = PrecoTotal;
     }
 
-    public void FazSom() {
-        System.out.println(Animal + " é um " + animaltype + " e tem " + idade + " anos");
+    public void setEmpresaCal(String NomedaEmpresa, Integer PrecoMensagem, Integer PrecoEMAIL, Integer Disparos, Integer PrecoTotal){
+        this.NomedaEmpresa = NomedaEmpresa;
+        this.PrecoMensagem = PrecoMensagem;
+        this.PrecoEMAIL = PrecoEMAIL;
+        this.Disparos = Disparos;
+        this.PrecoTotal = PrecoTotal;
+
     }
-}  
+
+
+
+    public void aplicarDesconto() {
+        if (PrecoMensagem >= 1000) {
+            descontoMensagem = (int) (PrecoMensagem * 0.10);
+            PrecoMensagem -= descontoMensagem / 2;
+        }
+        if (PrecoEMAIL >= 1000) {
+            Integer descontoEmail = (int) (PrecoEMAIL * 0.10);
+            PrecoEMAIL -= descontoEmail / 2;
+        }
+
+        
+    }
+
+    public Integer precoTotal() {
+        this.PrecoTotal = (PrecoMensagem * Disparos) + (PrecoEMAIL * Disparos) / descontoMensagem;
+        return PrecoTotal;
+    }
+
+    //Informaçoes uteis
+    public void exibirInfo() {
+        System.out.println("Nome da Empresa: " + NomedaEmpresa);
+        System.out.println("Preço por Mensagem: " + PrecoMensagem);
+        System.out.println("Preço por Email: " + PrecoEMAIL);
+        System.out.println("Número de Disparos: " + Disparos);
+
+        if (PrecoMensagem >= 1000){
+            System.out.println("Desconto de 10% aplicado no preço de SMS.");
+        }
+        if (PrecoEMAIL >= 1000){
+            System.out.println("Desconto de 10% aplicado no preço do email.");
+        }
+
+        
+
+        System.out.println("Preço Total: " + PrecoTotal);
+    }
+
+}
 
 public class Main {
     public static void main(String[] args) {
-        Animal[] animais = {
-            new Animal("Rex", "Cachorro", 5),
-            new Animal("Whiskers", "Gato", 3)
-        };
+        
+        //Informações da Empresa
+        Empresarial[] empresas = {
+            new Empresarial("Empresa A", 1200,1200, 10,5)
 
-        for (Animal  animal : animais) {
-            animal.FazSom();
-        }   
+        };
+        //Ate o limite de for
+        for (Empresarial empresa : empresas) {
+            empresa.aplicarDesconto();
+            empresa.precoTotal();
+            empresa.exibirInfo();
+        }
+       
     }
 }
+        
+    
+
+
+
+
+
